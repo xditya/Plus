97e8a669ebc01acff8d5d2fcd40a2831a31423ec
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-03-22 23:38:33 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,14 +6,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet."
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @A_Dark_Princ3\n"
-                     "`Bot created by:` [SnapDragon](tg://user?id=719877937)\n"
+    await alive.edit("**`HAA BHAI MAIN ZINDA HUN. NHI MRUNGA ITNI JALDI`**\n\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @buddhhu\n"
+                     "`Bot created by:` [꧁༺•|•𝙺𝚄𝙼𝙰𝚁•𝙰𝙼𝙸𝚃•|•༻꧂](tg://user?id=667805879)\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n"
-                     "[Deploy this userbot Now](https://github.com/Dark-Princ3/X-tra-Telegram)")
+                     "[Deploy this userbot Now](https://github.com/amitsharma123234/X-tra-Telegram)")
