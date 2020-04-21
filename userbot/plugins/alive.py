c823968278216ad43bc93dc472ce1c25d44cb638
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-04-21 06:27:54 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,7 +12,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet."
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("   **Welcome To X-Tra-TG**\n\n"
-    				 "` 🔸 Telethon version:` **{version.__version__}**\n` 🔹 Python:` **{python_version()}** \n` 🔸 Fork by:` @buddhhu\n"
+    				 "** 🔸 Telethon version:** {version.__version__}\n** 🔹 Python:** {python_version()} \n` 🔸 Fork by:` @buddhhu\n"
                      "` 🔹 Bot created by:` [༺•|•𝙺𝚄𝙼𝙰𝚁•𝙰𝙼𝙸𝚃•|•༻](tg://user?id=667805879)\n"
                      "` 🔸 Database Status:` **All OK 👌!**\n"
                      f"` 🔹 My peru owner`: {DEFAULTUSER}\n\n"
