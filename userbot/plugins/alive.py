29f9e6c07ccb4fe36a6d7db981a2a2c2b878abd1
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-04-21 06:46:04 +0530

    Now it's working

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,11 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet."
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("   **Welcome To X-Tra-TG**\n\n"
-    				 "** 🔸 Telethon version:** {version.__version__}\n** 🔹 Python:** {python_version()} \n` 🔸 Fork by:` @buddhhu\n"
+    await alive.edit("   **Welcome To X-Tra-TG** \n\n"
+    				 "`"
+    				 f" 🔸 Telethon version: {version.__version__} \n 🔹 Python: {python_version()} \n 🔸 Fork by: @buddhhu \n"
+    				 "`"
                      "` 🔹 Bot created by:` [༺•|•𝙺𝚄𝙼𝙰𝚁•𝙰𝙼𝙸𝚃•|•༻](tg://user?id=667805879)\n"
-                     "` 🔸 Database Status:` **All OK 👌!**\n"
-                     f"` 🔹 My peru owner`: {DEFAULTUSER}\n\n"
+                     "` 🔸 Database Status:` **All OK 👌!** \n"
+                     f"` 🔹 My owner:` {DEFAULTUSER}  \n\n"
                      "           [Deploy this userbot Now](https://github.com/amitsharma123234/X-tra-Telegram)")
