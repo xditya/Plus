9d4ede0c3c3e9651717c2af8bfdbae110a6b0217
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-04-21 06:22:34 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,8 +1,8 @@
 """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
 import asyncio
-from telethon import events
+from telethon import events, version
 from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
+from platform import python_version, uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
@@ -12,7 +12,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet."
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("   **Welcome To X-Tra-TG**\n\n"
-    				 "` 🔸 Telethon version:` **1.11.3**\n` 🔹 Python:` **3.6.10** \n` 🔸 Fork by:` @buddhhu\n"
+    				 "` 🔸 Telethon version:` **{version.__version__}**\n` 🔹 Python:` **{python_version()}** \n` 🔸 Fork by:` @buddhhu\n"
                      "` 🔹 Bot created by:` [༺•|•𝙺𝚄𝙼𝙰𝚁•𝙰𝙼𝙸𝚃•|•༻](tg://user?id=667805879)\n"
                      "` 🔸 Database Status:` **All OK 👌!**\n"
                      f"` 🔹 My peru owner`: {DEFAULTUSER}\n\n"
