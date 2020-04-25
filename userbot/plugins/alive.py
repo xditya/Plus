c683472ff80f28e8133f057c20004786d10886a6
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-04-25 09:41:16 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -16,7 +16,7 @@ async def amireallyalive(alive):
     				 f"    🔸 Telethon : {version.__version__} \n   🔹 Python: {python_version()} \n"
     				 "`"
                      "`  🔸 Fork by:` @buddhhu \n "
-                     "`🔹 Bot created by:` [༺•|•𝙺𝚄𝙼𝙰𝚁•𝙰𝙼𝙸𝚃•|•༻](tg://user?id=667805879)\n"
+                     "`🔹 Bot creator:` [༺•|•𝙺𝚄𝙼𝙰𝚁•𝙰𝙼𝙸𝚃•|•༻](tg://user?id=667805879)\n"
                      "`  🔸 Database Status:` **All OK 👌!** \n"
                      f"`   🔹 My owner:` {DEFAULTUSER}  \n"
                      "`    🔸 Join` @xtratgbot `For Help` \n\n"
