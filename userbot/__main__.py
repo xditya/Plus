705cdb4b18cc052b332997855e3498f7c7baac14
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-05-22 18:50:14 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Yay your userbot is officially working. Do .alive")
+print("Sab kuch pel diya. Ja khushi khushi gand mara...")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
