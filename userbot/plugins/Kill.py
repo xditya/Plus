242ce262537e0d27f4b00c919b0e826088322eb3
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-05-25 08:15:36 +0530

    Update Kill.py

--- userbot/plugins/Kill.py
+++ userbot/plugins/Kill.py
@@ -8,7 +8,7 @@ import asyncio
 
 
 
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True, allow_sudo=True))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
 
