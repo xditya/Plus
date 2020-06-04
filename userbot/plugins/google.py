1dd8b11ac5d7cf04f07412a239636f8a54d16077
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-06-04 19:43:32 +0530

    Update google.py

--- userbot/plugins/google.py
+++ userbot/plugins/google.py
@@ -9,7 +9,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-from google_images_download import google_images_download
+from userbot.google_images_download import google_images_download
 from uniborg.util import admin_cmd
 
 
