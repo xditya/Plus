a750ea99eb2c24e7cb576ebf7cf762a8c48003ff
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-06-15 18:40:18 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -145,47 +145,17 @@ def remove_plugin(shortname):
     except:
         raise ValueError
 
-def admin_cmd(**args):
+def admin_cmd(pattern=None, allow_sudo=False, **args):
     args["func"] = lambda e: e.via_bot_id is None
-    
-    pattern = args.get("pattern", None)
-    allow_sudo = args.get("allow_sudo", False)
 
-    # get the pattern from the decorator
     if pattern is not None:
-        if pattern.startswith("\#"):
-            # special fix for snip.py
-            args["pattern"] = re.compile(pattern)
-        else:
-            args["pattern"] = re.compile(Config.COMMAND_HAND_LER + pattern)
-
-    args["outgoing"] = True
-    # should this command be available for other users?
+        args["pattern"] = re.compile(Config.COMMAND_HAND_LER + pattern)
     if allow_sudo:
         args["from_users"] = list(Config.SUDO_USERS)
-        # Mutually exclusive with outgoing (can only set one of either).
-        args["incoming"] = True
-        del args["allow_sudo"]
-
-    # error handling condition check
-    elif "incoming" in args and not args["incoming"]:
+    else:
         args["outgoing"] = True
-
-    # add blacklist chats, UB should not respond in these chats
     args["blacklist_chats"] = True
-    black_list_chats = list(Config.UB_BLACK_LIST_CHAT)
-    if len(black_list_chats) > 0:
-        args["chats"] = black_list_chats
-
-    # check if the plugin should allow edited updates
-    allow_edited_updates = False
-    if "allow_edited_updates" in args and args["allow_edited_updates"]:
-        allow_edited_updates = args["allow_edited_updates"]
-        del args["allow_edited_updates"]
-
-    # check if the plugin should listen for outgoing 'messages'
-    is_message_enabled = True
-
+    args["chats"] = list(Config.UB_BLACK_LIST_CHAT)
     return events.NewMessage(**args)
 
 """ Userbot module for managing events.
