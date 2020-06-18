66824492adcaf08a9fb026ed32dbc6f64095ccf9
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-06-18 14:56:04 +0530

    Update img.py

--- userbot/plugins/img.py
+++ userbot/plugins/img.py
@@ -1,26 +1,24 @@
-"""Download & Upload Images on Telegram\n
-Syntax: `.img <Name>` or `.img (replied message)`
-\n Upgraded and Google Image Error Fixed by @NeoMatrix90 aka @kirito6969
-"""
-
 from userbot.google_images_download import googleimagesdownload
 import os
 import shutil
 from re import findall
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
+from userbot import CMD_HELP
 from userbot.config import Config
-
 
 @borg.on(admin_cmd(pattern="img ?(.*)"))
 async def img_sampler(event):
-    await event.edit("`Processing ...`")
+    await event.edit("`Processing....`")
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
     reply = await event.get_reply_message()
     if event.pattern_match.group(1):
         query = event.pattern_match.group(1)
-    elif reply:
+    elif reply.text:
         query = reply.message
     else:
-    	await event.edit("`What I am Supposed to Search u Dumb Ass(Donkey)`")
+    	await event.edit("`What I am Supposed to Search `")
     	return
         
     lim = findall(r"lim=\d+", query)
@@ -44,6 +42,11 @@ async def img_sampler(event):
     # passing the arguments to the function
     paths = response.download(arguments)
     lst = paths[0][query]
-    await event.client.send_file(await event.client.get_input_entity(event.chat_id), lst)
+    await event.client.send_file(await event.client.get_input_entity(event.chat_id), lst,reply_to=reply_to_id)
     shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
     await event.delete()
+
+    
+CMD_HELP.update({"images": "`.img <Name>` or `.img (replied message)`\
+    \nUSAGE: do google image search and sends 5 images." 
+})    
