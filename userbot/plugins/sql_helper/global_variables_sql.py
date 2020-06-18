56e44121239974f958f7803867650d0b5cd51960
amitsharma123234 <48654350+amitsharma123234@users.noreply.github.com>
2020-06-18 19:16:56 +0530

    Adding notes

---
+++ userbot/plugins/sql_helper/global_variables_sql.py
@@ -0,0 +1,7 @@
+# global variables will be assigned here
+# can be imported in any module to make life easier.
+from userbot.config import Config
+LOGGER = Config.PRIVATE_GROUP_BOT_API_ID
+BLACKLIST = Config.UB_BLACK_LIST_CHAT
+SYNTAX = {}
+SUDO_USERS = Config.SUDO_USERS
