5f3247e79f089f500575f42360d6b0d6d561b216
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-06-25 08:55:29 +0530

    Update design.py

--- userbot/plugins/design.py
+++ userbot/plugins/design.py
@@ -5,7 +5,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("join"))
+@borg.on(admin_cmd(pattern="join"))
 async def _(event):
     if event.fwd_from:
         return
@@ -21,7 +21,7 @@ async def _(event):
         await event.reply(mentions)
     await event.delete()
 
-@borg.on(admin_cmd("pay"))
+@borg.on(admin_cmd(pattern="pay"))
 async def _(event):
     if event.fwd_from:
         return
