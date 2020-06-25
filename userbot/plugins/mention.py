2b68b2b1a9f78eee4ced90b73579192f4a3be8ff
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-06-25 09:16:50 +0530

    Update mention.py

--- userbot/plugins/mention.py
+++ userbot/plugins/mention.py
@@ -5,7 +5,7 @@ from telethon import events
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("mention (.*)"))
+@borg.on(admin_cmd(pattern="mention (.*)"))
 async def _(event):
 	if event.fwd_from:
 		return	
