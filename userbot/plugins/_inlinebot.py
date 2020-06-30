bc1086d804053d0f161d1fafdb38eef84ede816c
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-06-30 13:57:01 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -3,9 +3,11 @@ import asyncio
 import json
 import random
 import re
-from telethon import events, errors, custom
+from telethon import events, errors, custom, functions, __version__
 from userbot import CMD_LIST
 import io
+import sys
+
 
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
@@ -17,7 +19,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                "© Userbot Help",
+                "© FRIDAYUserBot Help",
                 text="{}\nCurrently Loaded Plugins: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
@@ -36,7 +38,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own Userbot, and don't use mine!"
+            reply_pop_up_alert = "Get your own userbot, don't use others\n Join @xtratgplus for learning how to get userbot!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -55,7 +57,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own Userbot, and don't use mine!"
+            reply_pop_up_alert = "Get your own userbot, don't use others's\n Join @xtratgplus for learning how to get userbot!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
@@ -77,12 +79,20 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             © Userbot".format(plugin_name)
         try:
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-        except: 
-            halps = "Do .help {} to get the list of commands.".format(plugin_name)
-            await event.answer(halps, cache_time=0, alert=True)
+        except:
+            with io.BytesIO(str.encode(reply_pop_up_alert)) as out_file:
+                out_file.name = "{}.txt".format(plugin_name)
+                await bot.send_file(
+                    event.chat_id,
+                    out_file,
+                    force_document=True,
+                    allow_cache=False,
+                    caption=plugin_name
+                )
+
 
 def paginate_help(page_number, loaded_plugins, prefix):
-    number_of_rows = 5
+    number_of_rows = 10
     number_of_cols = 2
     helpable_plugins = []
     for p in loaded_plugins:
@@ -90,7 +100,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {}".format("😁", x),
+        "{} {} {}".format ("🇮🇳", x , "✔️"),
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
@@ -101,7 +111,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("◀️Previous", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("Next▶️", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("⏪Previous", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("Next⏩", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
