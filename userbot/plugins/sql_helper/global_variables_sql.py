6cb4838cb2ac1a832fa1b33b7878451245c3e80a
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-07-03 11:48:56 +0530

    update global_variables_sql.py

--- userbot/plugins/sql_helper/global_variables_sql.py
+++ userbot/plugins/sql_helper/global_variables_sql.py
@@ -5,16 +5,18 @@ import asyncio
 from telethon import events, version
 from telethon.tl.types import ChannelParticipantsAdmins
 from platform import python_version, uname
-from userbot import ALIVE_NAME
+from userbot import ALIVE_NAME, AUTOPIC_FONT_COLOUR, AUTOPIC_FONT
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet."
 
-ALIVE = f"**MADE IN 🇮🇳 , MADE FOR 🗺️** \n\n`🔸 Telethon : {version.__version__} \n🔹 Python: {python_version()} \n``🔸 Fork by:` @buddhhu \n`🔹 Bot creator:` [//•𝙺𝚞𝚖𝚊𝚛•𝙰𝚖𝚒𝚝•//](tg://user?id=667805879)\n`🔸 Database Status:` **All OK 👌!** \n`🔹 My owner:` {DEFAULTUSER}  \n`🔸 Join` @xtratgplus `For Help` \n\n                      [Deploy✔️](https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Famitsharma123234%2FX-tra-TG-plus&template=https%3A%2F%2Fgithub.com%2Famitsharma123234%2FX-tra-TG-plus)  \n\n   "
+ALIVE = f"**MADE IN 🇮🇳 , MADE FOR 🗺️** \n\n`🔸 Telethon : {version.__version__} \n🔹 Python: {python_version()} \n``🔸 Fork by:` {DEFAULTUSER} \n`🔹 Bot creator:` [//•𝙺𝚞𝚖𝚊𝚛•𝙰𝚖𝚒𝚝•//](tg://user?id=667805879)\n`🔸 Database Status:` **All OK 👌!** \n`🔹 My owner:` {DEFAULTUSER}  \n`🔸 Join` @xtratgplus `For Help` \n\n                      [Deploy✔️](https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Famitsharma123234%2FX-tra-TG-plus&template=https%3A%2F%2Fgithub.com%2Famitsharma123234%2FX-tra-TG-plus)  \n\n   "
 
-ALIVESTR = [f"█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█ \n█░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░█  █░║║║╠─║─║─║║║║║╠─░█ \n█░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░█ \n█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n\n{ALIVE}",
-	f"╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗\n║║║╠─║─║─║║║║║╠─\n╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝\n\n{ALIVE}",
-	f"╔╦═╦╗\n║║║║╠═╦╗╔═╦═╦══╦═╗\n║║║║║╩╣╚╣═╣╬║║║║╩╣\n╚═╩═╩═╩═╩═╩═╩╩╩╩═╝\n\n{ALIVE}",
-	f"╭╮╭╮╭╮╱╱╭╮\n┃┃┃┃┃┃╱╱┃┃\n┃┃┃┃┃┣━━┫┃╭━━┳━━┳╮╭┳━━╮\n┃╰╯╰╯┃┃━┫┃┃╭━┫╭╮┃╰╯┃┃━┫\n╰╮╭╮╭┫┃━┫╰┫╰━┫╰╯┃┃┃┃┃━┫\n╱╰╯╰╯╰━━┻━┻━━┻━━┻┻┻┻━━╯\n\n{ALIVE}"]
+ALIVESTR = [f"█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█ \n█░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░█  █░║║║╠─║─║─║║║║║╠─░█ \n█░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░█ \n█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n{ALIVE}",
+	f"╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗\n║║║╠─║─║─║║║║║╠─\n╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝\n{ALIVE}",
+	f"╔╦═╦╗\n║║║║╠═╦╗╔═╦═╦══╦═╗\n║║║║║╩╣╚╣═╣╬║║║║╩╣\n╚═╩═╩═╩═╩═╩═╩╩╩╩═╝\n{ALIVE}",
+	f"╭╮╭╮╭╮╱╱╭╮\n┃┃┃┃┃┃╱╱┃┃\n┃┃┃┃┃┣━━┫┃╭━━┳━━┳╮╭┳━━╮\n┃╰╯╰╯┃┃━┫┃┃╭━┫╭╮┃╰╯┃┃━┫\n╰╮╭╮╭┫┃━┫╰┫╰━┫╰╯┃┃┃┃┃━┫\n╱╰╯╰╯╰━━┻━┻━━┻━━┻┻┻┻━━╯\n{ALIVE}"]
+	
+AUTOPIC_FONT_COLOUR =str(AUTOPIC_FONT_COLOUR) if AUTOPIC_FONT_COLOUR else "255, 255, 255"
 	
 LOGGER = Config.PRIVATE_GROUP_BOT_API_ID
 BLACKLIST = Config.UB_BLACK_LIST_CHAT
