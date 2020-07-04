a703bb47e053c32e14db30307e196f0cd449d82a
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-07-04 09:20:54 +0530

    update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,15 +1,24 @@
 from telethon import events
 from datetime import datetime
-from uniborg.util import admin_cmd, edit_or_reply
+from userbot.utils import admin_cmd
 
+
+@borg.on(admin_cmd(pattern="ping"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("Pong!")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit("Pong!\n{}".format(ms))
 
 @borg.on(admin_cmd(pattern="ping", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    ed = await edit_or_reply(event, event.from_id, "...")
     start = datetime.now()
-    await ed.edit("Pong!")
+    a = await event.reply("Pong!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await ed.edit("**Pong!**\n`{}` __ms__".format(ms))
+    await a.edit("Pong!\n{}".format(ms))
