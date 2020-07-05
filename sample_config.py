8d7b05cc11fe1de4f80c1727a19c46312d978d56
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-07-05 08:56:26 +0530

    update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -190,19 +190,6 @@ def is_mongo_alive():
     return True
 
 
-# Init Redis
-# Redis will be hosted inside the docker container that hosts the bot
-# We need redis for just caching, so we just leave it to non-persistent
-REDIS = StrictRedis(host='localhost', port=6379, db=0)
-
-
-def is_redis_alive():
-    try:
-        REDIS.ping()
-        return True
-    except BaseException:
-        return False
-
 
 
 
