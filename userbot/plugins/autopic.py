bbb1b39fd9b870dbcb87970182f27b969b030410
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-07-07 08:44:14 +0530

    Added custom colours

--- userbot/plugins/autopic.py
+++ userbot/plugins/autopic.py
@@ -7,35 +7,32 @@ from pySmartDL import SmartDL
 from telethon.tl import functions
 import asyncio
 import shutil
-from userbot import AUTOPIC_COMMENT
 from userbot.utils import admin_cmd
-from userbot.plugins.sql_helper.global_variables_sql import AUTO_PIC_FONT
-
-FONT_FILE_TO_USE = f"fonts/{AUTO_PIC_FONT}"
-AUTOPIC_STR = str(AUTOPIC_COMMENT) if AUTOPIC_COMMENT else "Life Is too Short.\n And so is TG account."
+from userbot.plugins.sql_helper.global_variables_sql import FONT_FILE_TO_USE, AUTOPIC_STR, COLOUR
 
 @borg.on(admin_cmd(pattern="autopic"))
 async def autopic(event):
-    await event.edit("**Autopic** has been Enabled!!")
-    downloaded_file_name = "userbot/original_pic.png"
-    downloader = SmartDL(Var.DOWNLOAD_PFP_URL_CLOCK, downloaded_file_name, progress_bar=False)
-    downloader.start(blocking=False)
-    photo = "userbot/photo_pfp.png"
-    while not downloader.isFinished():
-        place_holder = None
-    counter = -0
-    while True:
-        shutil.copy(downloaded_file_name, photo)
-        im = Image.open(photo)
-        file_test = im.rotate(counter, expand=False).save(photo, "PNG")
-        current_time = datetime.now().strftime(f"Time: %H:%M \nDate: %d.%m.%y \n{AUTOPIC_STR}")
-        img = Image.open(photo)
-        drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-        drawn_text.text((95, 250), current_time, font=fnt, fill=(0, 0, 0))
-        img.save(photo)
-        file = await bot.upload_file(photo)
-        await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
-        await event.client(functions.photos.UploadProfilePhotoRequest( file))
-        os.remove(photo)
-        await asyncio.sleep(60)
+	await event.edit("**Autopic** has been Enabled!!")
+	downloaded_file_name = "userbot/original_pic.png"
+	downloader = SmartDL(Var.DOWNLOAD_PFP_URL_CLOCK, downloaded_file_name, progress_bar=False)
+	downloader.start(blocking=False)
+	photo = "userbot/photo_pfp.png"
+	while not downloader.isFinished():
+		place_holder = None
+		counter = -0
+		while True:
+			shutil.copy(downloaded_file_name, photo)
+			im = Image.open(photo)
+			file_test = im.rotate(counter, expand=False).save(photo, "PNG")
+			current_time = datetime.now().strftime(f"Time: %H:%M \nDate: %d.%m.%y \n{AUTOPIC_STR}")
+			img = Image.open(photo)
+			drawn_text = ImageDraw.Draw(img)
+			fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+			color = (COLOUR)
+			drawn_text.text((95, 250), current_time, font=fnt, fill=color)
+			img.save(photo)
+			file = await bot.upload_file(photo)
+			await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
+			await event.client(functions.photos.UploadProfilePhotoRequest( file))
+			os.remove(photo)
+			await asyncio.sleep(60)
