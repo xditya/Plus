81841046706b2e1a0b45902b3e06235591ca985f
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-07-09 09:10:04 +0530

    Changed sudo to sudoalive

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,7 +7,7 @@ async def alive(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit(random.choice(ALIVESTR))
         
-@borg.on(admin_cmd(pattern="sudo", allow_sudo=True))
+@borg.on(admin_cmd(pattern="sudoalive", allow_sudo=True))
 async def sudo(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.reply(random.choice(ALIVESTR))
