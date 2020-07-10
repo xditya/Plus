550b6c27d7f9c3165d2e70429a51c06b05ea6761
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-07-10 17:23:41 +0530

    update global_variables_sql.py

--- userbot/plugins/sql_helper/global_variables_sql.py
+++ userbot/plugins/sql_helper/global_variables_sql.py
@@ -8,11 +8,10 @@ from telethon import events, version
 from telethon.tl.types import ChannelParticipantsAdmins
 from platform import python_version, uname
 from userbot import ALIVE_NAME, AUTOPIC_FONT_COLOUR, AUTOPIC_FONT, AUTOPIC_COMMENT
-from userbot.plugins.sql_helper.global_variables_sql import get_readable_time as grt
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet."
 
-uptime = grt((time.time() - UpTime))
+uptime = get_readable_time((time.time() - UpTime))
 
 ALIVE = f"**MADE IN 🇮🇳 , MADE FOR 🗺️** \n\n`🔸 Telethon :` **{version.__version__}** \n`🔹 Python:` **{python_version()}** \n``🔸 Fork by:` @buddhhu \n`🔹 Bot creator:` [//•𝙺𝚞𝚖𝚊𝚛•𝙰𝚖𝚒𝚝•//](tg://user?id=667805879)\n`🔸 Plus+ Uptime:` **{uptime}** \n`🔹 My owner:` {DEFAULTUSER}  \n`🔸 Join` @xtratgplus `For Help` \n\n                      [Deploy✔️](https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Famitsharma123234%2FX-tra-TG-plus&template=https%3A%2F%2Fgithub.com%2Famitsharma123234%2FX-tra-TG-plus)  \n\n   "
 
