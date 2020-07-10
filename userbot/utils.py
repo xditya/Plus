c7f0eb3c6bd29d96ec5621c3f9a39a355007b230
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-07-10 17:34:25 +0530

    update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -12,6 +12,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import os
+import requests
 from userbot import bot
 from telethon import events
 from var import Var
@@ -295,6 +297,34 @@ async def progress(current, total, event, start, type_of_ps, file_name=None):
             await event.edit("{}\n{}".format(type_of_ps, tmp))
 
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+    
+    
 def humanbytes(size):
     """Input size in bytes,
     outputs in a human readable format"""
