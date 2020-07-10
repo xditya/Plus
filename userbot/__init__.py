8b8d7989fffc462fb05e0b71681cb0805f786a52
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-07-10 18:50:18 +0530

    update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -21,7 +21,6 @@ from var import Var
 
 UpTime = time.time()
 
-os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
     bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
