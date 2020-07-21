112a998ac3550089c76aeabf8980edc44906ed99
Amit Sharma <48654350+amitsharma123234@users.noreply.github.com>
2020-07-21 07:45:09 +0530

    update autopic.py

--- userbot/plugins/autopic.py
+++ userbot/plugins/autopic.py
@@ -12,27 +12,29 @@ from userbot.plugins.sql_helper.global_variables_sql import FONT_FILE_TO_USE, AU
 
 @borg.on(admin_cmd(pattern="autopic"))
 async def autopic(event):
-	await event.edit("**Autopic** has been Enabled!!")
 	downloaded_file_name = "userbot/original_pic.png"
 	downloader = SmartDL(Var.DOWNLOAD_PFP_URL_CLOCK, downloaded_file_name, progress_bar=False)
 	downloader.start(blocking=False)
 	photo = "userbot/photo_pfp.png"
 	while not downloader.isFinished():
 		place_holder = None
-		counter = -0
-		while True:
-			shutil.copy(downloaded_file_name, photo)
-			im = Image.open(photo)
-			file_test = im.rotate(counter, expand=False).save(photo, "PNG")
-			current_time = datetime.now().strftime(f"Time: %H:%M \nDate: %d.%m.%y \n{AUTOPIC_STR}")
-			img = Image.open(photo)
-			drawn_text = ImageDraw.Draw(img)
-			fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-			color = (COLOUR)
-			drawn_text.text((95, 250), current_time, font=fnt, fill=color)
-			img.save(photo)
-			file = await bot.upload_file(photo)
-			await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
-			await event.client(functions.photos.UploadProfilePhotoRequest( file))
+	counter = -30
+	while True:
+		shutil.copy(downloaded_file_name, photo)
+		im = Image.open(photo)
+		file_test = im.rotate(counter, expand=False).save(photo, "PNG")
+		current_time = datetime.now().strftime(f"Time: %H:%M \nDate: %d.%m.%y \n{AUTOPIC_STR}")
+		img = Image.open(photo)
+		drawn_text = ImageDraw.Draw(img)
+		fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+		drawn_text.text((95, 250), current_time, font=fnt, fill=(255, 255, 255))
+		img.save(photo)
+		file = await bot.upload_file(photo)  # pylint:disable=E0602
+		try:
+			await bot(functions.photos.UploadProfilePhotoRequest(file))
+			await event.edit("**Autopic has been enabled!!!**")
 			os.remove(photo)
+			counter -= 30
 			await asyncio.sleep(60)
+		except:
+			return
